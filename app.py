a6abbc1bd75d418d88299fea4054b09003d4676c
BrunoCTb <btogitndev@gmail.com>
2025-11-19 05:38:28 -0300

    feat: implement create bill and group page (only tkinter layout)

--- app.py
+++ app.py
@@ -26,6 +26,12 @@ class MainApp(ctk.CTk):
         self.dashboard_btn = ctk.CTkButton(self.sidebar, text="📊 Dashboard", command=self.show_dashboard)
         self.dashboard_btn.pack(fill="x", padx=20, pady=5)
 
+        self.bill_btn = ctk.CTkButton(self.sidebar, text="Adicionar conta", command=self.show_add_bill_page)
+        self.bill_btn.pack(fill="x", padx=20, pady=5)
+
+        self.bill_btn = ctk.CTkButton(self.sidebar, text="Adicionar grupo", command=self.show_add_group_page)
+        self.bill_btn.pack(fill="x", padx=20, pady=5)
+
         self.tasks_btn = ctk.CTkButton(self.sidebar, text="🗓️ Tarefas", command=self.show_tasks)
         self.tasks_btn.pack(fill="x", padx=20, pady=5)
 
@@ -46,6 +52,97 @@ class MainApp(ctk.CTk):
     def clear_main_frame(self):
         for widget in self.main_frame.winfo_children():
             widget.destroy()
+
+    def show_add_bill_page(self):
+        self.clear_main_frame()
+
+        title = ctk.CTkLabel(
+            self.main_frame,
+            text="Adicionar Nova Conta",
+            font=ctk.CTkFont(size=24, weight="bold")
+        )
+        title.pack(pady=15)
+
+        form = ctk.CTkFrame(self.main_frame, fg_color="transparent")
+        form.pack(pady=10, padx=20, fill="x")
+
+        self.bill_title = ctk.CTkEntry(form, placeholder_text="Título da conta")
+        self.bill_title.pack(pady=5, fill="x")
+
+        self.bill_value = ctk.CTkEntry(form, placeholder_text="Valor (ex: 120.90)")
+        self.bill_value.pack(pady=5, fill="x")
+
+        self.bill_type = ctk.CTkOptionMenu(form, values=["Gasto", "Ganho"])
+        self.bill_type.pack(pady=5, fill="x")
+
+        self.bill_due_date = ctk.CTkEntry(form, placeholder_text="Data de vencimento (dd/mm/aaaa)")
+        self.bill_due_date.pack(pady=5, fill="x")
+
+        self.bill_group = ctk.CTkOptionMenu(form, values=["Sem grupo"]) # get nos grupos do sqlite
+        self.bill_group.pack(pady=5, fill="x")
+
+        self.bill_card = ctk.CTkOptionMenu(form, values=["Nenhum cartão"]) # get nos cartoes do sqlite
+        self.bill_card.pack(pady=5, fill="x")
+
+        self.bill_description = ctk.CTkEntry(form, height=40, placeholder_text="descrição curta")
+        self.bill_description.pack(pady=5, fill="x")
+
+        radio_opt = ctk.IntVar();
+        radio_opt.set(0)
+
+        def set_radio_bill(opt):
+            if (opt == 1):
+                self.bill_total_installments.pack(pady=5, fill="x")
+                self.bill_paid_installments.pack(pady=5, fill="x")
+                return
+            
+            self.bill_total_installments.pack_forget()
+            self.bill_paid_installments.pack_forget()
+                
+        ctk.CTkLabel(form, text='Conta parcelada:').pack()
+
+        radio1 = ctk.CTkRadioButton(form, text="Não", variable=radio_opt, value=0, command=lambda: set_radio_bill(0))
+        radio2 = ctk.CTkRadioButton(form, text="Sim", variable=radio_opt, value=1, command=lambda: set_radio_bill(1))
+        radio1.pack()
+        radio2.pack()
+
+        self.bill_total_installments = ctk.CTkEntry(form, placeholder_text="Total de parcelas (ex: 12)")
+        self.bill_paid_installments = ctk.CTkEntry(form, placeholder_text="Parcelas pagas (ex: 3)")
+        
+        submit_btn = ctk.CTkButton(
+            self.main_frame,
+            text="Salvar Conta",             
+            fg_color="#2ecc71",
+            hover_color="#27ae60"
+        )
+        submit_btn.pack(pady=20)
+
+    def show_add_group_page(self): 
+        self.clear_main_frame()
+
+        title = ctk.CTkLabel(
+            self.main_frame,
+            text="Adicionar novo grupo",
+            font=ctk.CTkFont(size=24, weight="bold")
+        )
+        title.pack(pady=15)
+
+        form = ctk.CTkFrame(self.main_frame, fg_color="transparent")
+        form.pack(pady=10, padx=20, fill="x")
+
+        self.group_title = ctk.CTkEntry(form, placeholder_text="Nome do grupo")
+        self.group_title.pack(pady=5, fill="x")
+
+        self.group_description = ctk.CTkEntry(form, height=40, placeholder_text="descrição curta")
+        self.group_description.pack(pady=5, fill="x")        
+        
+        submit_btn = ctk.CTkButton(
+            self.main_frame,
+            text="Salvar grupo",             
+            fg_color="#2ecc71",
+            hover_color="#27ae60"
+        )
+        submit_btn.pack(pady=20)
 
     def show_main_area(self):
         self.clear_main_frame()
